957760e8145f1f0f1286de280500d97ffa012383
Luke Newhart <luke.newhart@edutech.org>
2026-04-13 16:31:18 -0400

    clean up

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var linearGauge16D5E17790E94862BEA55E4F2EE66BFE: IVisualPlugin;
-export default linearGauge16D5E17790E94862BEA55E4F2EE66BFE;
